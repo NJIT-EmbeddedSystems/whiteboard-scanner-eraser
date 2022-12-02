a310dce2c238ac92cf4db9d3e1fcea239e5128e0
rooq <rooq@rooq-mbp>
2022-12-02 11:46:27 -0500

    Update file organization

--- UI/UI.py
+++ UI/UI.py
@@ -2,18 +2,25 @@
 
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtGui import QPixmap
 
-class EmptyWindow(QWidget):
+class MainWindow(QWidget):
     def __init__(self):
        super().__init__()
        self.initializeUI()
 
     def initializeUI(self):
-        self.setGeometry(200,100,400,300)
+        self.setGeometry(200,100,400,300) # (x,y,width,height)
         self.setWindowTitle("Whiteboard Scanner/Eraser")
+        self.setUpMainWindow()
         self.show()
+
+    def setUpMainWindow(self):
+        hello_label = QLabel(self)
+        hello_label.setText("Hello")
+        hello_label.move(105,15)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)    # -d for debugging, otherwise empty for production
-    window = EmptyWindow()
+    window = MainWindow()
     sys.exit(app.exec())
