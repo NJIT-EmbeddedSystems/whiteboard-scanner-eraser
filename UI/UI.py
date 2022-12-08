8816df5cab57abb59d226aec881916e5951b83dd
Ivan Zhu <ivanzhu201@gmail.com>
2022-12-08 15:02:32 -0500

    Adjusted sizes and position of the checkboxes and text

--- UI/UI.py
+++ UI/UI.py
@@ -1,8 +1,8 @@
 # import PyQt6
 
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QCheckBox, QVBoxLayout
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QCheckBox, QVBoxLayout, QPushButton
+from PyQt6.QtGui import QPixmap, QFont
 from PyQt6.uic.properties import QtWidgets
 
 
@@ -19,14 +19,33 @@ class MainWindow(QWidget):
         self.show()
 
     def setUpMainWindow(self):
+        header_label = QLabel("Select options:", self)
+        header_label.setWordWrap(True)
+        header_label.move(15, 10)
+        header_label.setFont(QFont('Times', 20))
+
+        # scan checkbox
         scan_label = QLabel("Scan", self)
-        scan_label.move(15, 15)
+        scan_label.move(15, 55)
+        scan_label.setFont(QFont('Times', 18))
         scan_cb = QCheckBox(self)
-        scan_cb.move(50, 17)
+        scan_cb.move(84, 58)
+        scan_cb.setStyleSheet("""QCheckBox::indicator{width: 20px; height: 20px}""")
+
+        # erase checkbox
         erase_label = QLabel("Erase", self)
-        erase_label.move(15, 35)
+        erase_label.move(15, 95)
+        erase_label.setFont(QFont('Times', 18))
         erase_cb = QCheckBox(self)
-        erase_cb.move(50, 37)
+        erase_cb.move(84, 98)
+        erase_cb.setStyleSheet("""QCheckBox::indicator{width: 20px; height: 20px}""")
+
+        # Execute button
+        self.button = QPushButton("EXECUTE", self)
+        self.button.setFont(QFont('Times', 15))
+        self.button.setGeometry(140, 200, 120, 60)
+        # self.button.clicked.connect(self.execClicked())
+        # add buttonClicked() def
 
 
 if __name__ == '__main__':
