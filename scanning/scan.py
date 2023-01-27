594011c91e70f64c8f2507844035bc0fff51b882
Rushi A <69522459+RushiAth@users.noreply.github.com>
2023-01-27 13:12:25 -0500

    more simplifying

--- scanning/scan.py
+++ scanning/scan.py
@@ -1,45 +1,37 @@
 from stitch import stitching
 import cv2
+import glob
 
-#Getting raw photos (in jpg)
+# Getting raw photos (in jpg)
+path = "./testImages/whiteboard-test2/"
+allImages = glob.glob(path + "*")
 
-
-
-#Read images into CV2 image objects
-
-
-
-#Stitch the images
-
-
-
-
-#Save the images
-
-
-
-
-
-
-#Storing image paths and output paths
-allImages = [["./testImages/whiteboard-test2/20221118_121053.jpg", "./testImages/whiteboard-test2/20221118_121056.jpg", "./testImages/whiteboard-test2/20221118_121058.jpg"]]
-# tempPath = "./outputImages/firstTemp.jpg"
-# temp2Path = "./outputImages/secondTemp.jpg"
 outputPath = "./outputImages/whiteboardOutput.jpg"
 
-#Reading in images with OpenCV
+# allImages = [["./testImages/whiteboard-test2/20221118_121053.jpg", "./testImages/whiteboard-test2/20221118_121056.jpg", "./testImages/whiteboard-test2/20221118_121058.jpg"]]
+# tempPath = "./outputImages/firstTemp.jpg"
+# temp2Path = "./outputImages/secondTemp.jpg"
+
+
+
+
+# Read images into CV2 image objects
+imgList = list(map(cv2.imread, allImages))
+
 # img1 = cv2.imread(allImages[0][0])
 # img2 = cv2.imread(allImages[0][1])
 # img3 = cv2.imread(allImages[0][2])
 
-imgList = list(map(cv2.imread, allImages[0]))
 
+
+
+
+# Stitch the images vertically
 #TEMP 1
 # temp = stitching(imgList[0], imgList[1])
 
 #temp = removeBorder(tempPath)
 # cv2.imwrite(tempPath, temp)
-
 
 #TEMP 2
 # temp2 = stitching(imgList[1], imgList[2])
@@ -47,12 +39,19 @@ imgList = list(map(cv2.imread, allImages[0]))
 #temp2 = removeBorder(temp2Path)
 # cv2.imwrite(temp2Path, temp2)
 
-
 #FINAL
-firstOutput = stitching(imgList[0], imgList[1], imgList[2])
+output = stitching(imgList[0], imgList[1], imgList[2])
 
 #firstOutput = removeBorder(firstOutput)
-cv2.imwrite(outputPath, firstOutput)
+cv2.imwrite(outputPath, output)
+
+
+
+# Stitch the images horizontally
+
+
+
+
 
 #Used for repeating over length of whiteboard
 # for i in range(1, 10):
