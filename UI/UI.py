dcca4c25cb17ad2102edb71ee4ce0ba3632e07e7
Ivan Zhu <ivanzhu201@gmail.com>
2023-02-16 17:36:02 -0500

    Changed the look of the UI a little.

--- UI/UI.py
+++ UI/UI.py
@@ -4,8 +4,8 @@ import sys
 import buttons
 
 from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, \
-                             QCheckBox, QPushButton, QLineEdit, QHBoxLayout)
-# not used: qvboxlayout
+                             QCheckBox, QPushButton, QLineEdit, QVBoxLayout)
+
 # from PyQt6.QtGui import QPixmap
 from PyQt6.uic.properties import QtWidgets
 from PyQt6.QtCore import Qt
@@ -16,40 +16,46 @@ class MainWindow(QWidget):
         self.initializeUI()
 
     def initializeUI(self):
-        self.setGeometry(200, 100, 400, 300)  # (x,y,width,height)
+        self.setGeometry(200, 100, 400, 350)  # (x,y,width,height)
         self.setWindowTitle("Whiteboard Scanner/Eraser")
         self.setUpMainWindow()
         self.show()
 
     def setUpMainWindow(self):
-        layout = QHBoxLayout()
+        Vlayout = QVBoxLayout()
 
         header_label = QLabel("Select options:", self)
         header_label.setWordWrap(True)
-        header_label.move(15, 10)
+        Vlayout.addWidget(header_label)
 
         # scan checkbox
         scan_label = QLabel("Scan", self)
-        scan_label.move(15, 30)
+        Vlayout.addWidget(scan_label)
         scan_cb = QCheckBox(self)
-        scan_cb.move(60, 30)
+        Vlayout.addWidget(scan_cb)
 
         # erase checkbox
         erase_label = QLabel("Erase", self)
-        erase_label.move(15, 50)
+        Vlayout.addWidget(erase_label)
         erase_cb = QCheckBox(self)
-        erase_cb.move(60, 50)
+        Vlayout.addWidget(erase_cb)
 
         # save location entry
-        saveLocation_label = layout.addWidget(QLabel("Save Location:", self))
-        self.saveLocation_edit = layout.addWidget(QLineEdit(self))
+        saveLocation_label = QLabel("Save Location:", self)
+        Vlayout.addWidget(saveLocation_label)
+        self.saveLocation_edit = QLineEdit(self)
+        Vlayout.addWidget(self.saveLocation_edit)
 
         # Execute button
-        self.button = layout.addWidget(QPushButton("EXECUTE", self))
+        self.button = QPushButton("EXECUTE", self)
+        Vlayout.addWidget(self.button)
         # self.button.clicked.connect(self.execClicked())
         # add buttonClicked() def
 
-        self.setLayout(layout)
+        app.setStyleSheet("QLabel{font: 15pt;} QCheckBox::indicator{width: 20px; height: 20px}"
+                          "QPushButton{font: bold 20px} QLineEdit{height: 20px}")
+        self.setStyleSheet("background-color: lightblue;")
+        self.setLayout(Vlayout)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)  # -d for debugging, otherwise empty for production
