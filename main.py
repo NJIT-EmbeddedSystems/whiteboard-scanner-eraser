4bbe251d72ed05a1c715aaa266fee00ff2151ca7
rooq <rooq@users.noreply.github.com>
2023-03-08 11:27:15 -0500

    Add empty state defintions to 'main.py'

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from python-statemachine import StateMachine, State
 import physical.physical
 # import scanning.scanning
 # import UI.UI
+
+class whiteboardEraser(StateMachine):
+    # state definitions
+    start       = State(initial=True)
+    idle        = State()
+    move_fwd    = State()
+    idle2       = State()
+    process_img = State()
+    move_back   = State(final=True)
 
 exit()
