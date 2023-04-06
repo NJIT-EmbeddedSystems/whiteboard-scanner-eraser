f7b6ae7ae8368a35209129a997d482af2b9a9c4d
Ivan Zhu <ivanzhu201@gmail.com>
2023-04-06 18:50:48 -0400

    Changed the UI outlook, Added tabs, Added a temporary plain text edit for the log (might have to change it to something else)

--- UI/UI.py
+++ UI/UI.py
@@ -3,59 +3,66 @@
 import sys
 import buttons
 
-from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, \
-                             QCheckBox, QPushButton, QLineEdit, QVBoxLayout)
+from PyQt6.QtWidgets import (QApplication, QWidget, \
+                             QLabel, QCheckBox, QPushButton, QHBoxLayout,
+                             QTabWidget, QFormLayout, QGridLayout, QPlainTextEdit)
 
 # from PyQt6.QtGui import QPixmap
 from PyQt6.uic.properties import QtWidgets
 from PyQt6.QtCore import Qt
 
 class MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.initializeUI()
 
     def initializeUI(self):
-        self.setGeometry(200, 100, 400, 350)  # (x,y,width,height)
+        self.setGeometry(200, 100, 600, 450)  # (x,y,width,height)
         self.setWindowTitle("Whiteboard Scanner/Eraser")
         self.setUpMainWindow()
         self.show()
 
     def setUpMainWindow(self):
-        Vlayout = QVBoxLayout()
+        main_layout = QGridLayout(self)
+        self.setLayout(main_layout)
+        tab = QTabWidget(self)
+
+        # Main tab with scan, button, and log
+        main_tab = QWidget(self)
+        layout = QFormLayout()
+        main_tab.setLayout(layout)
 
         header_label = QLabel("Select options:", self)
         header_label.setWordWrap(True)
-        Vlayout.addWidget(header_label)
+        scan_check = QCheckBox(self)
+        erase_check = QCheckBox(self)
+        execute_button = QPushButton("EXECUTE")
+        empty_label = QLabel("", self)
 
-        # scan checkbox
-        scan_label = QLabel("Scan", self)
-        Vlayout.addWidget(scan_label)
-        scan_cb = QCheckBox(self)
-        Vlayout.addWidget(scan_cb)
+        layout.addRow(header_label)
+        layout.addRow("Scan ", scan_check)
+        layout.addRow("Erase ", erase_check)
+        layout.addRow(execute_button)
 
-        # erase checkbox
-        erase_label = QLabel("Erase", self)
-        Vlayout.addWidget(erase_label)
-        erase_cb = QCheckBox(self)
-        Vlayout.addWidget(erase_cb)
+        # Trying to add a space for the log
+        layout.addRow("", empty_label)
+        log_window = QPlainTextEdit()
+        layout.addRow(log_window)
 
-        # save location entry
-        saveLocation_label = QLabel("Save Location:", self)
-        Vlayout.addWidget(saveLocation_label)
-        self.saveLocation_edit = QLineEdit(self)
-        Vlayout.addWidget(self.saveLocation_edit)
+        # Live camera tab
+        camera_tab = QWidget(self)
+        layout = QHBoxLayout()
+        camera_tab.setLayout(layout)
 
-        # Execute button
-        self.button = QPushButton("EXECUTE", self)
-        Vlayout.addWidget(self.button)
-        # self.button.clicked.connect(self.execClicked())
-        # add buttonClicked() def
+        # Setting tab windows
+        tab.addTab(main_tab, "Scan / Erase")
+        tab.addTab(camera_tab, "Live Cameras")
+        main_layout.addWidget(tab, 0, 0, 2, 1)
 
+        # Altering background color, size of buttons, checkboxes, text, etc.
         app.setStyleSheet("QLabel{font: 15pt;} QCheckBox::indicator{width: 20px; height: 20px}"
-                          "QPushButton{font: bold 20px} QLineEdit{height: 20px}")
+                          "QPushButton{font: bold 22px}")
         self.setStyleSheet("background-color: lightblue;")
-        self.setLayout(Vlayout)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)  # -d for debugging, otherwise empty for production
